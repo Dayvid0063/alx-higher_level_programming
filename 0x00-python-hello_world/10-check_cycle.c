fea6280a6040c3e8a8e1abe51ee3e80a47218b02
Dayvid0063 <orjidavid0063@gmail.com>
2023-10-30 10:47:39 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
 	slow = list->next;
 	fast = list->next->next;
