fe9f119bcd3b69d39e0192a885845a84ce990f6e
Dayvid0063 <orjidavid0063@gmail.com>
2023-11-04 05:07:34 -0700

    5-no_c.py T5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     text = ""
     for u in my_string:
-        if (u != 'c') and (u != 'C'):
+        if u != "c" and u != "C":
             text += u
-        return (text)
+    return text
