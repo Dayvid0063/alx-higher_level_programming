d66f54e14ab848a6b7a18a681df91bf38f0f0f48
Dayvid0063 <orjidavid0063@gmail.com>
2023-11-04 06:34:24 -0700

    TASK8 new

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    new.u = sentence[0] if length > 0 else "None"
-    size = length, new.u
-    return size
+    first = sentence[0] if length > 0 else "None"
+    size = length, first
+    return(size)
