43eff4067da9d3750f546043e84c84860df07d1a
Dayvid0063 <orjidavid0063@gmail.com>
2023-11-08 08:58:29 -0800

    mandatory 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list(a_dictionary.keys()).sort()
+    for u in list(a_dictionary.keys()):
+        print("{}: {}".format(u, a_dictionary.get[u]))
