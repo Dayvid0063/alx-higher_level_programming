3df64c334b2eb4ae9135a55b2ead797e51b15f1a
Dayvid0063 <orjidavid63@gmail.com>
2023-11-20 04:17:41 -0800

    mandatory zero

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     val = 0
     for u in range(x):
         try:
-            print("()".format(my_list[u]), end="")
+            print("{}".format(my_list[u]), end="")
             val += 1
         except IndexError:
             break
