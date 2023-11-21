7d5c528f3026b86d8aa67e72f04b1df0918fe655
Dayvid0063 <orjidavid63@gmail.com>
2023-11-21 03:45:43 -0800

    task three

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class square definition"""
+"""class square definition."""
 
 
 class Square:
@@ -10,5 +10,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size * self.__size
