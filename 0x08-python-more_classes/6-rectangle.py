6041f640e1c3253c9997e170829ea4116f330ec4
Dayvid0063 <orjidavid63@gmail.com>
2023-11-27 16:22:26 +0100

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,11 +4,11 @@
 
 class Rectangle:
     """class rectangle"""
-    instance_count = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize rectangle"""
-        type(self).instance_count += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -68,5 +68,5 @@ class Rectangle:
 
     def __del__(self):
         """delete rectangle/print message"""
-        type(self).instance_count -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
