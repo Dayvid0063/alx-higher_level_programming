68a2b8cacef75085027c384dd82beb6567d5b24f
Dayvid0063 <orjidavid63@gmail.com>
2023-11-27 17:18:07 +0100

    task nine

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,17 +9,9 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initialize rectangle"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -28,7 +20,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """put width"""
-        if isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -41,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """put height"""
-        if isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -53,10 +45,10 @@ class Rectangle:
 
     def perimeter(self):
         """Return perimeter"""
-        if self.__height == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return self.__width * 2 + self.__height * 2
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
@@ -67,10 +59,10 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return (cls(size, size))
 
     def __str__(self):
         """rectangle representation with the # character"""
