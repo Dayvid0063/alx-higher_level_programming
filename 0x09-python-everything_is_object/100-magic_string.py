d7095b9ffe27eba35968d0f1765cbccde3ed3eb9
Dayvid0063 <orjidavid63@gmail.com>
2023-11-28 12:38:33 +0100

    tasks fixed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""class LockedClass with no class or object attribute"""
-
-
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ", ".join(["BestSchool" for u in range(magic_string.count)])
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-"""function returns a copy of a list"""
-
-
 def copy_list(mylist):
-    return (mylist.copy())
+    return mylist.copy()
