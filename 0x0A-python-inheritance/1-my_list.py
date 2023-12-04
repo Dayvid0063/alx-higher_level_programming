5c0d6094f46bdcc1306b70abc3d8bca9093663ac
Dayvid0063 <orjidavid63@gmail.com>
2023-12-04 20:17:59 +0100

    task one

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""MyList that inherits from list"""
+"""a class MyList that inherits from list"""
 
 
 class MyList(list):
     """list subclass"""
-    def __init__(self):
-        """initialization"""
-        super().__init__()
-
     def print_sorted(self):
-        """sorted list printed"""
+        """print sorted list"""
+
         print(sorted(self))
