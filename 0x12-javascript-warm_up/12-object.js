ec60eaa24b7511b3b7b5dcf4ab45428f1ec70c03
Dayvid0063 <orjidavid63@gmail.com>
2024-02-12 22:59:18 +0100

    mandatory complete

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Create an object with properties 'type' and 'value'
+const obj = {
+  type: 'object',
+  value: 12
+};
+
+// Log the state of the object to the console
+console.log(obj);
+obj.value = 89;
+console.log(obj);
