c70a63a6a5ba14f37c96712eafda119f3e17d0e4
Dayvid0063 <orjidavid63@gmail.com>
2024-02-12 23:24:14 +0100

    advanced 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,8 +2,8 @@
 
 // Define module that exports an obj
 module.exports = {
-  // The method callDayv takes two param
-  callDayv: function (v, theFunction) {
+  // The method callMeMoby takes two param
+  callMeMoby: function (v, theFunction) {
     // Loop v times
     for (let u = 0; u < v; u++) {
       // Execute the given function
