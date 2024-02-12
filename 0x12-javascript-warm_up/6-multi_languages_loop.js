4c9106dfe4bfd7f1529db48a1457e7abbcd5f2ce
Dayvid0063 <orjidavid63@gmail.com>
2024-02-12 20:40:52 +0100

    task 6 update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-for (const txt of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(txt);
-  }
+
+// Array of str to print
+const txt = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+
+// Loop through each element of the array and print it
+for (let u = 0; u < txt.length; u++) {
+    console.log(txt[u]);
+}
