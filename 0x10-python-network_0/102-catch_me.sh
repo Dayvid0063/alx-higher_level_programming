74f8fc604b32623ffc61793ec9fcdcffb28bbfa9
Dayvid0063 <orjidavid63@gmail.com>
2024-02-29 23:21:44 +0100

    advanced update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Makes request to 0.0.0.0:5000/catch_me that causes the server to respond 
-# With a message containing You got me!, in the body of the response
+# Makes request to 0.0.0.0:5000/catch_me that causes the server to respond with a message'You got me!'in the body
 curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: School"
