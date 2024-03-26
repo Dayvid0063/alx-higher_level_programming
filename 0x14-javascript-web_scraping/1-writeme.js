e05c7aaa95eb94867076bdc0e1ba2b487f53c523
Dayvid0063 <orjidavid63@gmail.com>
2024-03-26 17:16:17 +0100

    task 1 done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,7 @@
 
 const fs = require('fs');
 if (process.argv.length > 3) {
-  process.exit(1);
+  fs.writeFile(process.argv[2], process.argv[3], x => {
+    if (x) console.log(x);
+  });
 }
-
-// Writes to file
-fs.writeFile(process.argv[2], process.argv[3], x => {
-  if (x) console.log(x);
-});
