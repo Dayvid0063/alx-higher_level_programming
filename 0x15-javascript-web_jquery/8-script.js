c030704f83f44f8692a9f02011f050c29583a32e
Dayvid0063 <orjidavid63@gmail.com>
2024-04-03 16:29:26 +0100

    task 8/9

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 const link = 'https://swapi-api.alx-tools.com/api/films/?format=json';
 
-$.get(link, (data, stats) => {
+$.get(link, (data) => {
   data.results.forEach(film => {
     $('UL#list_movies').append(`<li>${film.title}</li>`);
   });
